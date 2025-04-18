bd11b9b6ae214be989d48e8112b70f37d278eb7c
Hauwa Eniola Abdulazeez <95659605+Heniola45@users.noreply.github.com>
2025-04-18 12:47:52 +0400

    reference name Header fix

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,7 +1,7 @@
 import About from "../about/About";
 import Contact from "../contact/Contact";
 import Footer from "../footer/Footer";
-import Header from "../Header/header";
+import Header from "../header/Header";
 import Projects from "../projects/Projects";
 import SubHeader from "../subHeader/SubHeader";
 
